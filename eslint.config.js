b013b5f7e1c80e69d1e80b1103174c23176ef1c8
Emmanuel Quarcoo <emmanuelq150@gmail.com>
2026-02-21 17:27:43 +0100

    initial

---
+++ eslint.config.js
@@ -0,0 +1,21 @@
+import js from "@eslint/js";
+import tseslint from "typescript-eslint";
+import eslintConfigPrettier from "eslint-config-prettier";
+import eslintPluginPrettier from "eslint-plugin-prettier";
+
+export default tseslint.config(
+  js.configs.recommended,
+  ...tseslint.configs.recommended,
+  eslintConfigPrettier,
+  {
+    plugins: {
+      prettier: eslintPluginPrettier,
+    },
+    rules: {
+      "prettier/prettier": "error",
+    },
+  },
+  {
+    ignores: ["dist/", "node_modules/"],
+  },
+);
